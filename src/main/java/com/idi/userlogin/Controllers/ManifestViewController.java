d04097ad20d9db5f8bfff39ada6f285e52fff6c0
craigt <Craigtbaum@gmail.com>
2021-06-11 20:26:56 -0400

    Added Item Info Tree For Manifest View

--- src/main/java/com/idi/userlogin/Controllers/ManifestViewController.java
+++ src/main/java/com/idi/userlogin/Controllers/ManifestViewController.java
@@ -2,13 +2,10 @@ package com.idi.userlogin.Controllers;
 
 import com.idi.userlogin.Handlers.ConnectionHandler;
 import com.idi.userlogin.Handlers.ControllerHandler;
-import com.idi.userlogin.Handlers.JsonHandler;
-import com.idi.userlogin.JavaBeans.Collection;
 import com.idi.userlogin.JavaBeans.Group;
 import com.idi.userlogin.JavaBeans.Item;
 import com.idi.userlogin.Main;
 import com.idi.userlogin.utils.DailyLog;
-import com.idi.userlogin.utils.ImgFactory;
 import com.idi.userlogin.utils.Utils;
 import com.jfoenix.controls.JFXTreeTableView;
 import com.jfoenix.controls.RecursiveTreeItem;
@@ -18,12 +15,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.StringConverter;
 import org.apache.commons.dbutils.DbUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.controlsfx.control.SearchableComboBox;
 import org.controlsfx.control.textfield.CustomTextField;
 
@@ -42,9 +41,7 @@ import java.util.stream.Collectors;
 
 import static com.idi.userlogin.JavaBeans.User.COMP_NAME;
 import static com.idi.userlogin.Main.fxTrayIcon;
-import static com.idi.userlogin.Main.jsonHandler;
 import static com.idi.userlogin.utils.DailyLog.scanLogID;
-import static com.idi.userlogin.utils.ImgFactory.IMGS.CHECKMARK;
 
 public class ManifestViewController extends BaseEntryController<BaseEntryController.EntryItem> implements Initializable {
 
@@ -52,6 +49,9 @@ public class ManifestViewController extends BaseEntryController<BaseEntryControl
     private final RecursiveTreeItem rootItem = new RecursiveTreeItem<>(treeRoot, RecursiveTreeObject::getChildren);
     private String uid;
     private String groupCol;
+
+    @FXML
+    private TreeView<String> itemInfo;
 
     @FXML
     private SearchableComboBox<Group> groupCombo;
@@ -76,9 +76,10 @@ public class ManifestViewController extends BaseEntryController<BaseEntryControl
             item.setupType(type);
             item.setComments(comments);
             item.getConditions().setAll(conditions);
-            final ObservableList items = group.getItemList();
-            items.add(item);
             insertHelper(item);
+            if (tree.getRoot().getChildren().isEmpty()) {
+                updateGroup(group);
+            }
             tree.getRoot().getChildren().add(newItem);
             typeCombo.getSelectionModel().selectFirst();
             createFoldersFromStruct(item);
@@ -88,6 +89,70 @@ public class ManifestViewController extends BaseEntryController<BaseEntryControl
                 itemCombo.getItems().remove(item);
             });
             resetFields();
+        }
+    }
+
+    public ObservableList<TreeItem<String>> getItemInfo(EntryItem item) {
+        Connection connection = null;
+        ResultSet set = null;
+        PreparedStatement ps = null;
+        ObservableList<TreeItem<String>> treeItems = FXCollections.observableArrayList();
+        try {
+            connection = ConnectionHandler.createDBConnection();
+            ps = connection.prepareStatement("SELECT * FROM `" + Main.jsonHandler.getSelJobID() + "` m WHERE id=? LIMIT 1");
+            ps.setInt(1, item.getId());
+            set = ps.executeQuery();
+            ResultSet cols = connection.getMetaData().getColumns(null, null, ControllerHandler.selJob.getName(), null);
+            List<String> colNames = new ArrayList<>();
+            while (cols.next()) {
+                String columnName = cols.getString("COLUMN_NAME");
+                colNames.add(columnName);
+            }
+            while (set.next()) {
+                for (int i = 0; i < colNames.size(); i++) {
+                    TreeItem col = new TreeItem(colNames.get(i));
+                    col.setExpanded(true);
+                    String data = set.getString(i + 1);
+                    TreeItem item_ = new TreeItem<String>(data);
+                    col.getChildren().add(item_);
+                    treeItems.add(col);
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Main.LOGGER.log(Level.SEVERE, "There was an error getting the groups from the db!", e);
+
+        } finally {
+            DbUtils.closeQuietly(set);
+            DbUtils.closeQuietly(ps);
+            DbUtils.closeQuietly(connection);
+        }
+
+        return treeItems;
+
+    }
+
+    public void updateGroup(Group group) {
+
+        Connection connection = null;
+        ResultSet set = null;
+        PreparedStatement ps = null;
+        try {
+            connection = ConnectionHandler.createDBConnection();
+            ps = connection.prepareStatement("UPDATE `sc_groups` SET employees='" + ConnectionHandler.user.getName() + "', started_on='" + LocalDateTime.now().toString() + "',status_id=(SELECT id FROM `sc_group_status` WHERE name='Scanning') WHERE id=?");
+            ps.setInt(1, group.getID());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Main.LOGGER.log(Level.SEVERE, "There was an error inserting a new group!", e);
+
+        } finally {
+            DbUtils.closeQuietly(set);
+            DbUtils.closeQuietly(ps);
+            DbUtils.closeQuietly(connection);
         }
     }
 
@@ -141,10 +206,42 @@ public class ManifestViewController extends BaseEntryController<BaseEntryControl
     @Override
     public void afterInitialize() {
         super.afterInitialize();
+        itemInfo.setCellFactory(e -> {
+            return new TreeCell<String>() {
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item != null) {
+                        //Parent
+                        if (!getTreeItem().isLeaf()) {
+                            setAlignment(Pos.CENTER_LEFT);
+                            setStyle("-fx-font-weight:bold;-fx-padding:8;-fx-font-size: 1.2em;");
+                        } else {
+                            setAlignment(Pos.CENTER_RIGHT);
+                            setStyle("-fx-font-weight:none;-fx-padding:4 40 0 0;-fx-font-size: 1.3em;");
+                        }
+                        setText(item);
+                    } else {
+                        setGraphic(null);
+                        setText(null);
+                    }
+                }
+            };
+        });
         tree.setEditable(false); //The View shouldn't be editable for Manifest Mode
-
+        itemInfo.setRoot(new TreeItem<String>());
         groupCombo.getSelectionModel().selectedItemProperty().addListener((ob, ov, nv) -> {
             groupSelectTask(nv, tree);
+        });
+
+        itemCombo.getSelectionModel().selectedItemProperty().addListener((ob, ov, nv) -> {
+            if (nv != null) {
+                itemInfo.getRoot().getChildren().clear();
+                ObservableList<TreeItem<String>> cols = getItemInfo(nv);
+                Platform.runLater(() -> {
+                    itemInfo.getRoot().getChildren().setAll(cols);
+                });
+            }
         });
 
         itemCombo.setCellFactory(e -> {
